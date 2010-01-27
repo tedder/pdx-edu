a3eeb9c751447ab81275e2fb1ef1685afc6893b6
Ted Timmons <ted-github@perljam.net>
2010-01-26 21:53:04 -0800

    proper 4-byte spacing

--- cs322/hw2/prog2-spacecount.s
+++ cs322/hw2/prog2-spacecount.s
@@ -15,6 +15,8 @@ f:
 # image 1, value 2
         movl	4(%esi), %ebx
 	imull	%ebx, %eax
+# get the 4-byte width correct by multiplying the size.
+	imull	$4, %eax
 # great. the size of image1 is now in eax.
 # Add the starting value of the image bitmap to get our bitmap ending point.
 addl	$8, %eax
@@ -29,22 +31,22 @@ addl	%esi, %eax
 	movl	$0, %ecx
 # compare against a space (char 32)
 testpixel:
-	cmp    $32, (%ebx)
+	cmpl    $32, (%ebx)
 # if it isn't equal, leave.
 	jne	notspace
 # okay, we have a space. Add to our overall count.
 yesspace:   ### only used as a debugging breakpoint
-	#addl	$3, %ecx
 	incl	%ecx
 
 notspace:
-	#addl	$4, %ebx
-	incl	%ebx
+	addl	$4, %ebx
+	#incl	%ebx
 
 # are we past the end? (need 'end' to be counted)
 	cmp	%eax, %ebx
 	jle	testpixel
 
+done:
 # toss the result into eax.
 	movl	%ecx, %eax
 # DEBUG: toss the *address* last checked onto eax
